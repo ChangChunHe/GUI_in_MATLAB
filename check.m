79292ae4bba7c7fd4921ee73a7d048249c76588a
hecc <731176792@qq.com>
2019-02-01 17:14:40 +0800

    Update check.m

--- check.m
+++ check.m
@@ -1,8 +1,8 @@
 function check(src,~)
 global N circum h
 tag = str2double(get(src,'Tag'));%把字符串转成数字
-row=ceil(tag/N);
-col = rem(tag,N);col(col==0)=N;boom=[];
+col=ceil(tag/N);
+row = rem(tag,N);row(row==0)=N;boom=[];
 I = imread('boom.jpg');squ_size = 500/N;
 I = imresize(I,floor([squ_size squ_size]*0.75));
 if circum(row,col)==9
@@ -13,7 +13,7 @@ if circum(row,col)==9
         end
     end
     for i=1:size(boom,1)
-        delete(h(sub2ind(size(circum),boom(i,2),boom(i,1))));
+        delete(h(sub2ind(size(circum),boom(i,1),boom(i,2))));
         tmp_row = boom(i,1); tmp_col = boom(i,2);
         drawnow
         hold on;image([squ_size*(tmp_col-1)+0.1*squ_size squ_size*tmp_col-0.1*squ_size],...
@@ -42,7 +42,7 @@ elseif circum(row,col)==0
         edge(circum(sub2ind(size(circum),edge(:,1),edge(:,2)))~=0,:) = [];
     end
     for i=1:size(cen,1)
-        set(h(sub2ind(size(circum),cen(i,2),cen(i,1))),'Style','text','string',[],'BackgroundColor',[205 197 191]/255);
+        set(h(sub2ind(size(circum),cen(i,1),cen(i,2))),'Style','text','string',[],'backgroundcolor',[205 197 191]/255);
     end
     edge = [];
     for ii = 1:size(cen,1)
@@ -51,13 +51,13 @@ elseif circum(row,col)==0
     edge = setdiff(edge,cen,'rows');
     edge(edge(:,1)<1|edge(:,1)>N|edge(:,2)<1|edge(:,2)>N,:) = [];
     for i=1:size(edge,1)
-        delete(h(sub2ind(size(circum),edge(i,2),edge(i,1))))
-        h((sub2ind(size(circum),edge(i,2),edge(i,1)))) = text((edge(i,2)-0.5)*squ_size,...
-            (N+0.5-edge(i,1))*squ_size,num2str(circum(edge(i,1),edge(i,2))),...
+        delete(h(sub2ind(size(circum),edge(i,1),edge(i,2))))
+        h((sub2ind(size(circum),edge(i,1),edge(i,2)))) = text((edge(i,2)-0.5)*squ_size,...
+            (N-edge(i,1)+0.5)*squ_size,num2str(circum(edge(i,1),edge(i,2))),...
             'fontsize',18,'HorizontalAlignment','center');
     end
 else %circum(row,col)~=9&&circum(row,col)~=0
-    delete(h(sub2ind(size(circum),col,row)))
-    h((sub2ind(size(circum),col,row))) = text((col-0.5)*squ_size,(N+0.5-row)*squ_size,num2str(circum(row,col)),...
+    delete(h(sub2ind(size(circum),row,col)))
+    h((sub2ind(size(circum),row,col))) = text((col-0.5)*squ_size,(N+0.5-row)*squ_size,num2str(circum(row,col)),...
         'fontsize',18,'HorizontalAlignment','center');
 end
