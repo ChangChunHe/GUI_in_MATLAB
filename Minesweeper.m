1b4c3a2d928047bc088f14eadc70803ae93796a5
hecc <731176792@qq.com>
2019-02-01 14:34:22 +0800

    remodify some hidding bugs

--- Minesweeper.m
+++ Minesweeper.m
@@ -17,7 +17,7 @@ line(repmat(0:squ_size:squ_size*N,2,1),repmat([0;squ_size*N],1,N+1),'color','k')
 initial=zeros(N);
 for i=1:N
     for j=1:N
-        initial(i,j) = sign(rand-0.7);
+        initial(i,j) = sign(rand-0.85);
         if initial(i,j) == -1
             initial(i,j) = 0;
         else
@@ -43,10 +43,10 @@ for counter=1:N^2
     jcols = rem(counter,N);
     jcols(jcols==0) = N;
     irows = ceil(counter/N);
-    position = [50-squ_size+squ_size*jcols squ_size*N+100-squ_size*irows squ_size squ_size];
+    position = [50-squ_size+squ_size*jcols+squ_size*0.05 ...
+        squ_size*N+100-squ_size*irows+squ_size*0.05 squ_size*0.9 squ_size*0.9];
     h(counter)=uicontrol( 'FontSize',18,'FontWeight','bold','Position',position,...
-        'Style','pushbutton','Tag',num2str(counter),'callback', @check,...
-        'BackgroundColor',[1 1 1]);
+        'Style','pushbutton','Tag',num2str(counter),'callback', @check);
 end
 
 % add a flag number
--- check.m
+++ check.m
@@ -4,8 +4,7 @@ tag = str2double(get(src,'Tag'));%把字符串转成数字
 row=ceil(tag/N);
 col = rem(tag,N);col(col==0)=N;boom=[];
 I = imread('boom.jpg');squ_size = 500/N;
-I = imresize(I,floor([squ_size squ_size]));
-
+I = imresize(I,floor([squ_size squ_size]*0.8));
 if circum(row,col)==9
     for i=1:N
         for j=1:N
@@ -17,7 +16,6 @@ if circum(row,col)==9
         delete(h(sub2ind(size(circum),boom(i,2),boom(i,1))));
         tmp_row = boom(i,1); tmp_col = boom(i,2);
         drawnow
-        
         hold on;image([squ_size*(tmp_col-1) squ_size*tmp_col],[N*squ_size-(tmp_row-1)*squ_size N*squ_size-tmp_row*squ_size],I)
     end
     %cancel uicontrols
@@ -26,7 +24,6 @@ if circum(row,col)==9
             set(h(i),'callback','')
         end
     end
-    
 elseif circum(row,col)==0
     cen = [row col];
     near=[1 0;0 1;0 -1;-1 0];
@@ -53,15 +50,13 @@ elseif circum(row,col)==0
     edge = setdiff(edge,cen,'rows');
     edge(edge(:,1)<1|edge(:,1)>N|edge(:,2)<1|edge(:,2)>N,:) = [];
     for i=1:size(edge,1)
-        %         set(h(sub2ind(size(circum),edge(i,2),edge(i,1))),'visible','off');%'Style','text','BackgroundColor',[205 197 191]/255,'string',num2str(circum(edge(i,1),edge(i,2)))
         delete(h(sub2ind(size(circum),edge(i,2),edge(i,1))))
         h((sub2ind(size(circum),edge(i,2),edge(i,1)))) = text((edge(i,2)-0.5)*squ_size,...
             (N+0.5-edge(i,1))*squ_size,num2str(circum(edge(i,1),edge(i,2))),...
-            'BackgroundColor',[205 197 191]/255,'fontsize',18,'HorizontalAlignment','center');
+            'fontsize',18,'HorizontalAlignment','center');
     end
-    
 else %circum(row,col)~=9&&circum(row,col)~=0
     delete(h(sub2ind(size(circum),col,row)))
     h((sub2ind(size(circum),col,row))) = text((col-0.5)*squ_size,(N+0.5-row)*squ_size,num2str(circum(row,col)),...
-        'fontsize',18,'BackgroundColor',[205 197 191]/255,'HorizontalAlignment','center');
+        'fontsize',18,'HorizontalAlignment','center');
 end
