03cb6bdd247662141a4321c128e48a9e4f925fd4
hecc <33366949+ChangChunHe@users.noreply.github.com>
2019-02-11 14:49:50 +0800

    add some descriptions

--- coding_review.m
+++ coding_review.m
@@ -1,5 +1,7 @@
 clear
 clc
+%% generate 0-1 matrix
+% version 1
 N = 10;
 initial=zeros(N);
 bomnumber = 0;
@@ -15,6 +17,7 @@ for i=1:N
 end
 
 %version 2
+% 实际上我们只需要先随机生成一个均匀随机矩阵, 然后使得大于0.8的为1, 其他的为0就可以实现所需的要求了.
 initial_copy = rand(N);
 initial_copy(initial_copy > 0.8) = 1;
 initial_copy(initial_copy < 1) = 0;
